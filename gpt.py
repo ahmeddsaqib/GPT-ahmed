417d17f6ecc92738b0b16447bf917f3149ce836f
ahmeddsaqib <ahmedsaqib661@gmail.com>
2026-08-22 16:37:08 +0500

    fixed a bug, the model training is now improved

--- gpt.py
+++ gpt.py
@@ -190,4 +190,3 @@ for iter in range(max_iters):
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('is_ts_peak.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
